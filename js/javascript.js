01b117423a4350729360cf677c4ee87e014bbef1
VovaDev777 <polvovater@gmail.com>
2023-07-22 13:00:32 +0200

    slider

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+// $('.autoplay').slick({
+//     dots: false,
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     dotsClass: "my-dots",
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+// });
+$('.fade').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
